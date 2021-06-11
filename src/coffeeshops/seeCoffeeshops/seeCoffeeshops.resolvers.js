61f587a4ab62e184741c89021d1906b1d64220d6
PJ <wooritobi@gmail.com>
2021-06-12 00:35:38 +0900

    seeCoffeeshops schema modified

--- src/coffeeshops/seeCoffeeshops/seeCoffeeshops.resolvers.js
+++ src/coffeeshops/seeCoffeeshops/seeCoffeeshops.resolvers.js
@@ -1,10 +1,16 @@
 import client from "../../client";
 
 export default {
+    CoffeeShopResult: {
+        totalShops: () => client.coffeeShop.count(),
+    },
+    
     Query:{
-        seeCoffeeshops: (_,{page=1}) => client.coffeeShop.findMany({
-            take: 2,
-            skip: (page-1) * 2
-        })
-    }
-}
+        seeCoffeeshops: (_,{page=1}) => ({
+            coffeeShops: client.coffeeShop.findMany({
+            take: 3,
+            skip: (page-1) * 3
+            }),
+        }),
+    },
+};    
